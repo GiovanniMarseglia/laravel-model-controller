40707ae790d8e2a70aba734468cca3d43a4a8c06
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-03-14 13:38:37 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comics</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+
+    <!-- header -->
+
+    <h1>{{dd($movie[0]["title"])}}</h1>
+</body>
+</html>
